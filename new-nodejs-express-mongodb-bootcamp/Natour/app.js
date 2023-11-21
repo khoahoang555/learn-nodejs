8422aec675d1bba4cb82e9fa343e9097ed4a3b72
khoa pham <khoa.pham@tpssoft.com>
2023-11-21 17:52:48 +0700

    Update source code

--- new-nodejs-express-mongodb-bootcamp/Natour/app.js
+++ new-nodejs-express-mongodb-bootcamp/Natour/app.js
@@ -7,8 +7,12 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // MIDDLEWAVES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, resp, next) => {
   console.log('Hello from the middleware');
