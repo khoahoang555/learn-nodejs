518813877baa278f3dbf408a8317319197082259
khoahoang555 <khoahoangyahoo@gmail.com>
2023-11-23 07:27:56 +0700

    Update source code

--- new-nodejs-express-mongodb-bootcamp/Natour/controllers/tourController.js
+++ new-nodejs-express-mongodb-bootcamp/Natour/controllers/tourController.js
@@ -1,10 +1,25 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
+
+exports.aliasTopTours = (req, resp, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty';
+  next();
+}
 
 exports.getAllTours = async (req, resp) => {
   try {
-    const tours = await Tour.find();
+    // EXECUTE QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
+    // SEND RESPONSE
     resp.status(200).json({
       status: 'success',
       results: tours.length,
@@ -88,3 +103,95 @@ exports.deleteTour = async (req, resp) => {
     });
   }
 };
+
+exports.getTourStats = async (req, resp) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } }
+      // }
+    ]);
+    resp.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+  } catch (err) {
+    resp.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+}
+
+exports.getMonthlyPlan = async (req, resp) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: {
+          numTourStarts: -1
+        }
+      },
+      {
+        $limit: 12
+      }
+    ]);
+
+    resp.status(200).json({
+      status: 'success',
+      data: {
+        plan
+      }
+    });
+  } catch (err) {
+    resp.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+}
